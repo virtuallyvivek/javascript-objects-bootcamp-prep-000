d2f195b0ee1988e069f40ce44fba875479a1347c
Catherine Straus <vnair611@gmail.com>
2020-06-22 23:42:42 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,7 @@
-var playlist = { artistName : 'songTitle'};
+
 
 function updatePlaylist (playlist, artistName, songTitle){
+  var playlist = { artistName : 'songTitle'};
   return Object.assign ({}, playlist, {[artistName]: songTitle})
 }
 
